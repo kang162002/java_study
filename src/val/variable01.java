87bdd3d343a2b077ada3a551fb96397b1e6dd101
kang162002 <6334540@naver.com>
2025-04-15 08:56:01 +0900

    java 변수개념, 변수타입

--- src/val/variable01.java
+++ src/val/variable01.java
@@ -1,90 +1,80 @@
 package val;
 
-public class variable01 {
+public class Variable01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		//����  ( ���� ������ �� �ִ� ���� ) ex) �ڽ�, �ٱ���
-			
-		int number1;	 //int : ������ ���� �ٱ��� �̸� : number1 
-		int floor; 		// ������ ���� ���� ���� ( �����̸� : floor )
+		
+		
+		//변수 (값을 저장할 수 있는 공간)  ex) 박스, 바구니 ...
+		
+		//변수 선언
+		
+		int number1;	// int:정수형 숫자     바구니이름:number1
+		int floor;   	// 정수형 숫자 담을 공간 (공간의 이름:floor)
 		
 //		int x;
-//		int y;		
+//		int y;
 //		int z;
-			//�̸� �ߺ� �ȵ� 
-		int x,y,z;
 		
-		int score; 		//���� ����
-		int point = 50; // ���� ���� + �� ���� -> ���� �ʱ�ȭ
+		int x, y, z;
 		
-//		int score = 99; // �ߺ��� ������ ���Ұ�
+		int score; //변수 선언
+		int point = 50;	//변수 선언 + 값 저장 -> 변수 초기화
 		
+		// int score = 99;  //중복된 변수명 사용불가
 		
-		score = 90; //�� ����
+		score = 90;	//값 저장
 		score = 80;
-		score = 70; //�ڵ�� ���� ���������� �Ʒ��ִ°��� ����ȴ�.
+		score = 70;
+		
+		//hour = 15;  //변수 선언 이전에 사용불가
+		int hour = 14;	//정수형숫자 변수 선언 + 값 저장 -> 초기화   공간(변수)이름:hour
+		
+		//seconds = 31; //선언 안한 변수 사용 불가
+		
+		int minute = 14;
+		
+		minute = 21;
+		
+		minute = minute + 7;
+		score = point + 20;
+		
+		int totalScore = point + point + score + score;
+		//					50		50		70		70
+		
+		//출력문
+		// System.out.println(괄호안에있는 값/문자 출력);	
+		System.out.println("안녕하세요");
+		System.out.println(500);
+		System.out.println(totalScore);
+		System.out.println(2*60 + 43);  // sysout  -> Ctrl + Space 자동완성 
+		System.out.println(hour + "시 " + minute + "분" );
 		
 		
-		//hour = 15; //���� ���� ������ ���Ұ�
-		int hour = 14; 	// 	���������� ���� ���� + �� ���� -> �ʱ�ȭ �����̸�:hour(�ð�)
+		x = 10;
+		y = x;
+		z = x + y;
+		
+		//x:10 y:10 z:20
+		
+		int a = 10;
+		int b = 20;
+		
+		// a:10  b:20
 
+		//목표 :  서로 값 바꾸기
+		// 		a:20  b:10
 		
-		// seconds = 31 // ���� ���� ���� ��� �Ұ�
-			
-			int minute =14 ; // 
-			
-			minute = 21;
-			minute = minute + 7;   
-			score = point + 20;
-		// int ���� + ���� + ���� + ����; ������ �ջ�
-			int totalscore = point + point + score + score;
-		//						50		50		70		70 
-			// ��¹�
-
-//			System.out.println(��ȣ�ȿ��ִ� ��/���� ���);
-			System.out.println("�ȳ��ϼ���");
-			System.out.println(500);
-			System.out.println(totalscore);
-			System.out.println(2*60 + 43); // sysout - > Ctrl + Space �ڵ��ϼ�
-			System.out.println(hour + "��" + minute + "��" );
-			
-			x = 10;
-//			x = 10 ���� 
-			y = x;
-//			y = x�ǰ��� y�� ������ 10
-			z = x + y;
-//			z = x + y�� x�� y�� ���� ��ó 20�� �����°� z�� ��ġ�°� �����ش�
-			// x:10 y:10 z:20
-			
-			int a = 10;
-			int b = 20;
-			
-			// a:10 b:20
-			
-			//��ǥ : ���� �� �ٲٱ�
-			// 		a:20  b:10
-			/* �ٲٱ� ���� ���̽�
-			a = b; // 	a:20 b:20
-			b = a ;//	a:20 b:20
-			*/
-			
-			int c = a; 	//a:10 b:20 c:10  	c�� a���ִ� ���ڰ��� �����Ѵ�, (a:10)
-				a = b; 	//a:20 b:20 c:10 	a�� b���ִ� ���ڰ��� �����Ѵ�, (b:20 > b:0)
-				b = c; 	//a:20 b:10 c:10 	b�� c���ִ� ���ڰ��� �����Ѵ�, (c:0 > b:10)
-				
-			/* 	�ڹ� ���� ����,�Ǽ�,������ �����ϴ� ��8���� �⺻ Ÿ���� ����
-			 *	����Ÿ�� : ��(truth) : ����(fail) �� ������ ���� 
-			 */
-			
-			
-				
-				
-				
-			
-				
-			
+		/* 바꾸기 실패 케이스
+		a = b;	//a:20 b:20
+		b = a;  //a:20 b:20
+		*/
+		
+		int c = a; //a:10 b:20 c:10
+		a = b;     //a:20 b:20 c:10
+		b = c;	   //a:20 b:10 c:10
+		
 	}
 
 }
--- src/val/variable02.java
+++ src/val/variable02.java
@@ -1,62 +1,52 @@
 package val;
 
-import java.awt.image.ByteLookupTable;
-
-public class variable02 {
+public class Variable02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			
-		//	2, 8, 10, 16 ���� - ������
-		int num2 = 0b1010; 	// 2���� : 10
-		int num8 = 012; 	// 8���� : 10
-		int num10 = 10; 	// 10����: 10
-		int num16 = 0x30; 	// 16����: 48
 		
-		//���ǻ���: ��ȭ��ȣ ���ڸ�
-		//int num = 010; ��ȭ��ȣ 010�� �ƴ϶� 8���� 10 ���� �ν��Ѵ�.
-		//�������� "010"
+		//2, 8, 10, 16 진수	-> 정수형
+		int num2 = 0b1010;	//2진수 : 10
+		int num8 = 012;  	//8진수 : 10
+		int num10 = 10;		//10진수: 10 
+		int num16 = 0x30;   //16진수: 48
 		
-		// char ���� Ÿ���� ������ �ϳ��� ���ڸ� �����Ҽ��ִ� Ư���� ����
+		//주의사항: 전화번호 앞자리
+		//int num = 010;  전화번호 010이 아니라 8진수 10 으로 인식
+		//문자형태 "010"
 		
-		char ch1 = 123;  //������ ���� char : 0 ~ 65535(�����ڵ�) // �ڵ�ǥ�� �´� ���ڷ� �ٲ��
-		char ch2 = 'B';  // ���� �Ѱ� 	'���� ����ǥ'
-//		char ch3 = 'AB'; //���� �ΰ� 	��� �Ұ�
-//		char ch3 = "A";  // ���� �Ѱ� 	'��������ǥ'  		"ū����ǥ" ���Ұ�
+		char ch1 = 123;	//정수형 변수 char		
+		char ch2 = 'B'; //문자 한개 '작은따옴표'
+		//char ch3 = 'AB';//두개 사용 불가
+		//char ch3 = "A"; //문자 한개 '작은따옴표'   "큰따옴표 사용 불가
 		
-		System.out.println(ch1);	// �����ڵ�ǥ 123 �� ��Ÿ���� ���� { // �ƽ�Ű�ڵ� 
+		System.out.println(ch1); //유니코드표 123 이 나타내는 문자 {
 		System.out.println(ch2);
 		
 		String str1 = "AB";
-		String str2 = "C";	//'C' ��������ǥ ���� �ϳ��� -> char Ÿ�Կ� ���
+		String str2 = "C";		//'C' 작은따옴표 문자하나는 -> char 타입에 사용
 		
-		String str3 = "�ѱ۹��ڿ�";
+		String str3 = "한글문자열";
 		String str4 = "01012345678";
-		String str5 = "010-123-5678";
+		String str5 = "010-1234-5678";
 		
 		System.out.println(str1);
 		System.out.println(str3 + str5);
 		
-		String menu = "���� \"�ܹ��Ÿ�\"�����Ѵ�."; // ���� "�ܹ���"�� �����Ѵ�.
-				// 	  (   )      	(      ) ū����ǥ�� �����ϴ� ���ڿ��� ������ ���������� �ִ´�, 
+		String menu = "나는 \"햄버거\"를 좋아한다.\\";	// 나는 "햄버거"를 좋아한다.
+				//	  (    )    (         )
 		System.out.println(menu);
 		
-		System.out.println("Hello\tworld\newlcome");
-								// �������� \t�� ������ �����̽���ȿ��  \b�� ĭ �ٲٱ�
+		System.out.println("Hello\tworld!\nWelcome");
 		
-		double db1 = 12.12; // �Ǽ���
-		double db2 = 0.1212e2; // 0.1212 * 10������  -> 12.12 
-		float f1 = 12.12f; // float Ÿ�Կ� �Ǽ� ���ڰ� �����Ҷ��� �ڿ� f�� ���δ�.
+		double db1 = 12.12; //실수형 
+		double db2 = 0.1212e2; // 0.1212 * 10의제곱	 -> 12.12
+		float f1 = 12.12f; //float 타입에 실수 숫자값 저장할때는 뒤에 f를 붙인다
 		
-		// ������ ��/���� true / flase 
+		//논리형 참/거짓 true/false
 		boolean stop = true;
 		boolean start = false;
-		boolean isHungry = true;
-		
-		
-		
-		
-		
+		boolean isHungry = true; 
 	}
 
 }
