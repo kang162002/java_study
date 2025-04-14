c12036ceca36fdc61b3da9e89523be06f08a9551
kang162002 <6334540@naver.com>
2025-04-14 12:33:35 +0900

    자바 시작,gtitgnore추가

---
+++ src/study/start/Hello.java
@@ -0,0 +1,26 @@
+package study.start;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		System.out.println("Hello World!!");
+		
+		// 	주석
+		// 	실행 (Run)은 : Ctrl + F11 누르면 나온다.
+		
+		//	단축키 :	Ctrl + /  쓰거나
+		//			Ctrl + Shift + C 
+		// 	세미콜론 ( ; ) 실행문 끝에는 세미콜론을 붙여야한다.		
+		/* 
+		 * 	여러줄 주석
+		 * 	
+		 */
+			
+		
+		
+		
+	}
+
+}
