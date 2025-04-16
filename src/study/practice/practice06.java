5207b5cda596754385914f00db5001239bce3845
kang162002 <6334540@naver.com>
2025-04-16 15:07:53 +0900

    연습5.6 입출력&연산자

---
+++ src/study/practice/practice06.java
@@ -0,0 +1,74 @@
+package study.practice;
+
+import java.io.StringBufferInputStream;
+import java.util.Scanner;
+
+public class practice06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		//1번 삼각형의 밑변과 높이를 입력 계산 출력하게요
+		// 1번 사다리꼴 윗변 밑변 높이를 받아서 넒이를 출력하세요.
+//		Scanner scanner = new Scanner(System.in);
+		
+		
+//			System.out.print("삼각형 밑변 입력하세요 : ");
+//			int base3 = scanner.nextInt();
+//			System.out.print("삼각형 높이를 입력하세요 : ");
+//			int height3 = scanner.nextInt();
+//			 
+//			double area3 =(double)base3 * height3 / 2;
+//			System.out.printf("삼각형의 넒이는 %.2f 입니다.\n", area3); 
+		
+		
+			
+			
+//			System.out.print("사다리꼴 윗변 입력하세요 : ");
+//			int top = scanner.nextInt();			// int top으로 scanner.nextInt 를 저장 
+			
+//			System.out.print ("사다리꼴 밑변 입력하세요 : ");
+//			int base = scanner.nextInt();
+			
+//			System.out.print("사다리꼴 높이 입력하세요 : ");
+//			int height = scanner.nextInt();
+			
+//			double area = (top + base) * height / 2.0; 
+			// 저장한 두개를 double 로 새로운 저장소 area를 만들어 ( 저장소 + 저장소 ) 입력  이후 * 곱하기 
+			
+//			System.out.printf("사다리꼴의 얾이는 %.2f 입니다.\n : ", area);
+
+//			2번
+			
+//			int x =10;
+//			int y =20;
+//			System.out.println("x=" +x+ "y="+y);
+			//x와 y를 바꾼작업
+
+//			int z = x; //x값 임시보관
+//			x = y;
+//			y = z;
+			
+//			System.out.println();
+		
+//			3번
+//			System.out.println("한명당 받는 과자수 : " + (100/24) );
+//			System.out.println("남는 과자수 : " + (100%24) );
+			//계산 24* 4 = 96 개 100 4개 남는다
+		
+			//4번
+			
+			Scanner scanner = new Scanner(System.in);
+			
+			System.out.print("세자리의 정수를 입력 : ");
+			int num1 = scanner.nextInt();
+			
+			System.out.println("백의자리 : " +(num1/100) );
+			System.out.println("십의자리 : " +(num1/10%10) );
+			System.out.println("일의자리 : " + (num1%10));
+			
+			
+	}
+
+}
