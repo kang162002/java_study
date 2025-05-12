e0f973ee16802c5223238492cd28107a0d6f2e98
kang162002 <6334540@naver.com>
2025-05-12 14:45:06 +0900

    연습42 클래스구조

---
+++ src/study/practice/practice42/EduCenter.java
@@ -0,0 +1,16 @@
+package study.practice.practice42;
+
+import java.util.List;
+
+public class EduCenter {
+		
+	// 직원 목록
+	List<Staff> empList;
+	//호실(방) 목록
+	List<Room> roomList;
+
+}
+// 방 ( Room )
+// 강의실
+// 휴게실
+// 사무실
